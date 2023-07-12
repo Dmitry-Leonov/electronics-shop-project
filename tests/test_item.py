c89b68bb47bab7a1a60d5afdfb95e3e0f94fe53c
Dmitry Leonov <leonov.do72@gmail.com>
2023-07-12 20:39:55 +0300

    Тесты написаны

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,16 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200_000
+    assert isinstance(item.calculate_total_price(), float)
+    item.price = 0
+    assert item.calculate_total_price() == 0
+
+
+def test_apply_discount(item):
+    item.apply_discount()
+    assert item.price == 10000
+    item.pay_rate = 0.8
+    item.apply_discount()
+    assert item.price == 8000
